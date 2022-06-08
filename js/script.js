ff4e4fe05a0a8573ca57d99a108209bbd1860886
ameer <ameer.em.89@gmail.com>
2022-06-08 09:40:46 +0300

    add the readme of this file and the change of the script

--- js/script.js
+++ js/script.js
@@ -1,17 +1,9 @@
-if(localStorage.getItem('data')!=""){
-    arr=JSON.parse(localStorage.getItem('data'));
-}
-else{
-    arr=[];
-}
-var arr = [];
-document.getElementById("btn").addEventListener('click', func);
-function func() {
-  let x1 =document.getElementById("nameproduct");
-  let x2 =document.getElementById("price");
-  let x3 =document.getElementById("selects");
-  let x4 =document.getElementById("img");
-  let x5 =document.getElementById("table");
+let arr = [];
+document.getElementById("btn").addEventListener("click", () => {
+  let x1 = document.getElementById("nameproduct");
+  let x2 = document.getElementById("price");
+  let x3 = document.getElementById("selects");
+  let x4 = document.getElementById("img");
   if (x1.value == "" || x1.value == null) {
     alert("please enter a product name");
   } else if (x2.value == "" || x2.value == null) {
@@ -23,42 +15,78 @@ function func() {
   } else if (x4.value == "") {
     alert("put a product image");
   } else {
-    let ele1 = document.createElement("label");
-    let ele2 = document.createElement("label");
-    let ele3 = document.createElement("label");
-    let ele4 = document.createElement("img");
-    let ele5 = document.createElement("td");
-    let ele6 = document.createElement("td");
-    let ele7 = document.createElement("td");
-    let ele8 = document.createElement("td");
-    let ele9 = document.createElement("tr");
-    ele1.textContent = x1.value;
-    ele2.textContent = x2.value;
-    ele3.textContent = x3.value;
-    ele4.src = x4.value;
-    ele4.className = "images";
-    ele5.appendChild(ele1);
-    ele6.appendChild(ele2);
-    ele7.appendChild(ele3);
-    ele8.appendChild(ele4);
-    ele9.appendChild(ele5);
-    ele9.appendChild(ele6);
-    ele9.appendChild(ele7);
-    ele9.appendChild(ele8);
-    x5.appendChild(ele9);
     let y = {
       productname: x1.value,
       productprice: x2.value,
       producttype: x3.value,
+      imageurl: x4.value,
     };
     arr.push(y);
-    x1.value="";
-    x2.value="";
-    x3.value="";
-    x4.value="";
-
+    inserttotable(y);
     localStorage.setItem("data", JSON.stringify(arr));
+
+    x1.value = "";
+    x2.value = "";
+    x3.value = "";
+    x4.value = "";
+  }
+});
+function inserttotable(y) {
+  let x5 = document.getElementById("table");
+  let ele1 = document.createElement("label");
+  let ele2 = document.createElement("label");
+  let ele3 = document.createElement("label");
+  let ele4 = document.createElement("img");
+  let removeBtn = document.createElement("input");
+
+  let ele5 = document.createElement("td");
+  let ele6 = document.createElement("td");
+  let ele7 = document.createElement("td");
+  let ele8 = document.createElement("td");
+  let removeTd = document.createElement("td");
+
+  let ele9 = document.createElement("tr");
+  ele1.textContent = y.productname;
+  ele2.textContent = y.productprice;
+  ele3.textContent = y.producttype;
+  ele4.src = y.imageurl;
+  ele4.className = "images";
+  ele5.appendChild(ele1);
+  ele6.appendChild(ele2);
+  ele7.appendChild(ele3);
+  ele8.appendChild(ele4);
+  removeTd.appendChild(removeBtn);
+  ele9.appendChild(ele5);
+  ele9.appendChild(ele6);
+  ele9.appendChild(ele7);
+  ele9.appendChild(ele8);
+  ele9.appendChild(removeTd);
+  x5.appendChild(ele9);
+  removeBtn.type = "button";
+  removeBtn.value = "remove";
+  removeBtn.addEventListener("click", () => {
+    let deleteIndex = arr.findIndex((val, y) => {
+      if (
+        val.name == y.productname &&
+        val.price == y.productprice &&
+        val.type == y.producttype &&
+        val.imageURL == y.imageurl
+      )
+        return true;
+    });
+    arr.splice(deleteIndex, 1);
+
+    localStorage.setItem('data', JSON.stringify(arr));
+    ele9.remove();
+  });
+}
+function fetchLocalStorage() {
+  if (localStorage.getItem("data")) {
+    arr = JSON.parse(localStorage.getItem("data"));
+    for (const prod of arr) {
+      inserttotable(prod);
+    }
   }
 }
 
-
+fetchLocalStorage();
---
+++ readme.md
@@ -0,0 +1,32 @@
+# My project
+In this project I prepared a system which allows the user to add different products in a Cart products.
+
+# Description
+
+This repository consists of different files:
+
+1. Html file: contains the structre of the page- html code.
+2. Css File: describes the design of the page.
+3. Javascript File: contains functions which constitute the engine of the website.
+
+## The Product
+In order to add a product to my products Cart you must fill the following:
+1. product name
+2. product price 
+3. product type
+4. product image/ the url of the image
+
+## My Cart 
+In this Cart you will see all the details of the product you added in the same line of the table. 
+at the end point in the line of the table you will see a remove button which if you click on it the line itself will be removed. 
+
+# Installing
+
+1. Copy my https repository: <a href="https://fullstack-alfanar.github.io/task-1-ameer/" target="_blank">Task 1-Ameer Emran</a>
+2. choose any terminal
+3. Git Clone- work locally with your computer
+4. Copy Repo URL
+5. cd (The name of the repository)
+6. type a command code .
+
+# All Rights Reserved- By Ameer Emran: Fullstack student developer: 09.05.2022
